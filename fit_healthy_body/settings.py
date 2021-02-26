cc76bfa21e819c8a86ac275e17feab662adcde96
chasakara <tinashechasakara@gmail.com>
2021-02-26 13:21:42 +0000

    changes to includes

--- fit_healthy_body/settings.py
+++ fit_healthy_body/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'home',
     'products',
     'bag',
+    'checkout',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,9 @@ TEMPLATES = [
         },
     },
 ]
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
