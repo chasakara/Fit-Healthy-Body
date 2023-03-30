64e74c1661c4a24a6a486c5b9a8c8173ad6f2ef8
chasakara <tinashechasakara@gmail.com>
2023-03-30 20:29:47 +0000

    changed host app settings

--- fit_healthy_body/settings.py
+++ fit_healthy_body/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['fit-healthy-body.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['https://fithealthybody.onrender.com', 'localhost']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
