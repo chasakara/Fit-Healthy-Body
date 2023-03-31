2b4ec140a6a3c1c5cef29b285caf1362d8d8527d
chasakara <tinashechasakara@gmail.com>
2023-03-31 06:32:18 +0000

    added

--- fit_healthy_body/settings.py
+++ fit_healthy_body/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['https://fithealthybody.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0,1']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
